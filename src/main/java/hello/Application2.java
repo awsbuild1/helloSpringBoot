eb990f38623b4f7aab8af32cb273ac3c7e2dc5c4
PGE\uxc6 <uxc6@pge.com>
2018-03-15 12:02:31 -0700

    war packaging

---
+++ src/main/java/hello/Application2.java
@@ -0,0 +1,95 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+	HelloController.class,
+    HelloResponse.class
+})
+public class Application2 extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application2.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+    
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .select()
+            .paths(Predicates.not(PathSelectors.regex("/error.*")))
+            .build();
+    }
+
+    @Component
+    @Primary
+    public class CustomObjectMapper extends ObjectMapper {
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public CustomObjectMapper() {
+            setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            enable(SerializationFeature.INDENT_OUTPUT);
+        }
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Say Hello API")
+            .description("'Say hello API' description")
+            .contact(new Contact("umesh", "https://github.com/awsbuild1/helloSpringBoot", "uxc6@pge.com"))
+            .license("PGE's(?)Apache License Version 2.0")
+            .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+            .version("2.0")
+            .build();
+    }
+
+}
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,23 +8,43 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 
 @RestController
 public class HelloController {
 	
-	private static final Logger LOGGER = Logger.getLogger(HelloController.class);
+	private static final Logger LOGGER = Logger.getLogger(HelloController.class);	
 
 	//No Swagger here!
     @RequestMapping("/hello")
     
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/hello/ip")
+    public String getIp(HttpServletRequest request) {
+    	String s = "unresolved";
+		try {
+			s = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}// request.getHeader("HTTP_X_FORWARDED_FOR");
+    	LOGGER.info(s);
+        return "IP: " + s;
     }
     
 
